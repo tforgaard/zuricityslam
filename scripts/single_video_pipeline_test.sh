bc46fdb0bcbfbed0d2b01849d956feab9959065e
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-04-15 22:01:34 +0200

    make script executable

--- scripts/single_video_pipeline_test.sh
+++ scripts/single_video_pipeline_test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-bsub -o short_walk_zurich_test.out -n 8 -R "rusage[mem=4096,ngpus_excl_p=1]" "python3 src/mapping/single_video_pipeline.py 
-                                    \ --dataset datasets/short_walk_zurich --outputs outputs/short_walk_zurich --num_loc 5"
+bsub -o short_walk_zurich_test.out -n 16 -R "rusage[mem=4096,ngpus_excl_p=2]" "python3 src/mapping/single_video_pipeline.py --dataset datasets/short_walk_zurich --outputs outputs/short_walk_zurich --num_loc 10"
