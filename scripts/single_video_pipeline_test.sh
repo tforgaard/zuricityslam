72a7e61f78757d5c0b8e4270bf650465fe20b758
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-04-16 14:18:11 +0200

    make script a bit more usable

--- scripts/single_video_pipeline_test.sh
+++ scripts/single_video_pipeline_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 PAIRING=sequential+retrieval
-bsub -o short_walk_zurich_test_${PAIRING}_fps2.out -n 16 -R "rusage[mem=4096,ngpus_excl_p=2]" "python3 src/mapping/single_video_pipeline.py --dataset datasets/short_walk_zurich --outputs outputs/short_walk_zurich_${PAIRING}_fps2 --num_loc 5 --window_size 5 --pairing ${PAIRING}"
+DATASET=loop_walk_zurich
+bsub -o ${DATASET}_test_${PAIRING}_fps2.out -n 16 -R "rusage[mem=4096,ngpus_excl_p=2]" "python3 src/mapping/single_video_pipeline.py --dataset datasets/${DATASET} --outputs outputs/${DATASET}_${PAIRING}_fps2 --num_loc 5 --window_size 5 --pairing ${PAIRING}"
