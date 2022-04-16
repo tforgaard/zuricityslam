54404b2c93cbf70c0b8f10dd96926981f1929b6f
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-04-16 11:08:12 +0200

    added sequential mapping

--- scripts/single_video_pipeline_test.sh
+++ scripts/single_video_pipeline_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bsub -o short_walk_zurich_test.out -n 16 -R "rusage[mem=4096,ngpus_excl_p=2]" "python3 src/mapping/single_video_pipeline.py --dataset datasets/short_walk_zurich --outputs outputs/short_walk_zurich --num_loc 10"
+bsub -o short_walk_zurich_test_sequential_fps2.out -n 16 -R "rusage[mem=4096,ngpus_excl_p=2]" "python3 src/mapping/single_video_pipeline.py --dataset datasets/short_walk_zurich --outputs outputs/short_walk_zurich_sequential_fps2 --num_loc 10"
