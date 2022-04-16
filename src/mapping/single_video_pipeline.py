fdbd29bdde6f07d0d577a7fc657d821dce859060
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-04-16 11:08:12 +0200

    added sequential mapping

--- src/mapping/single_video_pipeline.py
+++ src/mapping/single_video_pipeline.py
@@ -9,16 +9,19 @@ import matplotlib.pyplot as plt
 
 from hloc import extract_features, match_features, reconstruction, visualization, pairs_from_retrieval
 from hloc.utils import viz
+from src.mapping import pairs_from_sequence
 
 # Run SfM reconstruction from scratch on a set of images.
 
 # TODO for model merging utilize the loop closure by doing every ten seconds do retrevial to other sequences
 
+confs = {'pairing': ['sequential', 'retrieval', 'sequential+retrieval']}
 
-def main(base_dir, dataset, outputs, num_loc):
+
+def main(base_dir, dataset, outputs, num_loc, pairing):
 
     # define paths
-    images = base_dir / dataset / 'images'
+    images = base_dir / dataset / 'images-fps2'
     outputs = base_dir / outputs
 
     sfm_pairs = outputs / f'pairs-netvlad{num_loc}.txt'
@@ -35,13 +38,33 @@ def main(base_dir, dataset, outputs, num_loc):
     # sfm_pairs = outputs / f'pairs-db-covis{num_covis}.txt'
     # loc_pairs = outputs / f'pairs-query-netvlad{num_loc}.txt'
 
-    # ## Find image pairs via image retrieval
-    # We extract global descriptors with NetVLAD and find for each image the most similar ones. For smaller dataset we can instead use exhaustive matching via `hloc/pairs_from_exhaustive.py`, which would find $\frac{n(n-1)}{2}$ images pairs.
+    # ## Find image pairs either via sequential pairing, image retrieval or eventually both
 
-    # TODO increase number of num mathced
-    retrieval_path = extract_features.main(retrieval_conf, images, outputs)
-    # TODO make a new pairs_from_retrieval that takes sequenciality (±10 images) and also does retrieval but NOT on the pairs already found from sequenciality
-    pairs_from_retrieval.main(retrieval_path, sfm_pairs, num_matched=num_loc)
+    if pairing in confs['pairing']:
+        if pairing == 'sequential':
+            def get_image_names(image_path):
+                image_list = list(Path(image_path).glob('*.jpg'))
+                image_list = [str(il.name) for il in image_list]
+                image_list = sorted(list(set(image_list)))
+                return image_list
+
+            image_list = get_image_names(images)
+            print(image_list)
+
+            pairs_from_sequence.main(
+                sfm_pairs, image_list, features=None, window_size=num_loc)
+        elif pairing == 'retrieval':
+            # We extract global descriptors with NetVLAD and find for each image the most similar ones.
+            # TODO increase number of num mathced
+            retrieval_path = extract_features.main(
+                retrieval_conf, images, outputs)
+            # TODO make a new pairs_from_retrieval that takes sequenciality (±10 images) and also does retrieval but NOT on the pairs already found from sequenciality
+            pairs_from_retrieval.main(
+                retrieval_path, sfm_pairs, num_matched=num_loc)
+        elif pairing == 'retrieval+sequential':
+            raise NotImplementedError(f"TODO: implement retrieval+sequential")
+    else:
+        raise ValueError(f'Unknown pairing method')
 
     # ## Extract and match local features
 
@@ -70,7 +93,10 @@ if __name__ == "__main__":
                         help='Path to the output directory, default: %(default)s')
     parser.add_argument('--num_loc', type=int, default=20,
                         help='Number of image pairs for loc, default: %(default)s')
+    parser.add_argument('--pairing', type=str, default='sequential',
+                        help=f'Pairing method, default: %(default)s', choices=confs['pairing'])
     args = parser.parse_args()
+    # ## Run mapping
     model, outputs, images = main(**args.__dict__)
 
     # ## Visualization
