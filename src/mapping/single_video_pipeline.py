f10592105233d34fbef0d239113d8bfc9b493d13
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-04-19 12:50:16 +0200

    minor fixes

--- src/mapping/single_video_pipeline.py
+++ src/mapping/single_video_pipeline.py
@@ -10,7 +10,7 @@ from pycolmap import CameraMode
 
 from hloc import extract_features, match_features, reconstruction, visualization, pairs_from_retrieval
 from hloc.utils import viz
-import pairs_from_sequence
+from hloc import pairs_from_sequence
 
 # Run SfM reconstruction from scratch on a set of images.
 
@@ -32,7 +32,7 @@ def main(base_dir, dataset, outputs, window_size, num_loc, pairing):
     feature_conf = extract_features.confs['superpoint_aachen']
     matcher_conf = match_features.confs['superglue']
 
-    outputs.mkdir(exist_ok=True, parents=True, mode=777)
+    outputs.mkdir(exist_ok=True, parents=True, mode=0o777)
     # ref_sfm = outputs / 'sfm_superpoint+superglue'
     # ref_sfm_scaled = outputs / 'sfm_sift_scaled'
     # query_list = outputs / 'query_list_with_intrinsics.txt'
