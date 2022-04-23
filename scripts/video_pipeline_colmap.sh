23e9a7cd72e16b6fe1020f3f5b45e6c232785fbe
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-04-23 18:14:34 +0200

    minor changes

--- scripts/video_pipeline_colmap.sh
+++ scripts/video_pipeline_colmap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 BASE=/cluster/project/infk/courses/252-0579-00L/group07
 PAIRING=sequential+retrieval
-DATASET=4k/loop_walk_zurich
+DATASET=4k/long_walk_zurich
 
 mkdir -p ./logs/${DATASET}
 source ./scripts/colmap_startup.sh
-
-bsub -J hloc_pipeline -o ./logs/${DATASET}/test_${PAIRING}_fps2.out -W 2:00 -n 16 -R "rusage[mem=4096,ngpus_excl_p=2]" "python3 src/mapping/single_video_pipeline.py --dataset datasets/${DATASET} --outputs outputs/${DATASET}_${PAIRING}_fps2 --num_loc 5 --window_size 5 --pairing ${PAIRING}"
-bsub -J colmap_mapping -w "ended(hloc_pipeline)" -o ./logs/${DATASET}/test_${PAIRING}_fps2_colmap_mapping.out -W 12:00 -n 16 -R "rusage[mem=4096,ngpus_excl_p=1]" "mkdir -p ${BASE}/outputs/${DATASET}_${PAIRING}_fps2/sfm_superpoint+superglue/colmap && colmap mapper --database_path ${BASE}/outputs/${DATASET}_${PAIRING}_fps2/sfm_superpoint+superglue/database.db --image_path ${BASE}/datasets/${DATASET}/images-fps2 --output_path ${BASE}/outputs/${DATASET}_${PAIRING}_fps2/sfm_superpoint+superglue/colmap --Mapper.num_threads 16 --Mapper.ba_global_use_pba=1 --Mapper.ba_global_pba_gpu_index=0"
+#TODO add snapshots!
+bsub -J hloc_pipeline -o ./logs/${DATASET}/test_${PAIRING}_fps2.out -W 24:00 -n 16 -R "rusage[mem=4096,ngpus_excl_p=2]" "python3 src/mapping/single_video_pipeline.py --dataset datasets/${DATASET} --outputs outputs/${DATASET}_${PAIRING}_fps2 --num_loc 7 --window_size 6 --pairing ${PAIRING}"
+bsub -J colmap_mapping -w "ended(hloc_pipeline)" -o ./logs/${DATASET}/test_${PAIRING}_fps2_colmap_mapping.out -W 24:00 -n 32 -R "rusage[mem=4096,ngpus_excl_p=1]" "mkdir -p ${BASE}/outputs/${DATASET}_${PAIRING}_fps2/sfm_superpoint+superglue/colmap && colmap mapper --database_path ${BASE}/outputs/${DATASET}_${PAIRING}_fps2/sfm_superpoint+superglue/database.db --image_path ${BASE}/datasets/${DATASET}/images-fps2 --output_path ${BASE}/outputs/${DATASET}_${PAIRING}_fps2/sfm_superpoint+superglue/colmap --Mapper.num_threads 16 --Mapper.ba_global_use_pba=1 --Mapper.ba_global_pba_gpu_index=0"
