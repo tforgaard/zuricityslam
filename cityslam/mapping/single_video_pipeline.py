39eca774eb6d39c5fcd4fc16bf0130a332b6dc99
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-04-26 23:40:35 +0200

    done some refactoring, made project installable, made a global pipeline demo

--- cityslam/mapping/single_video_pipeline.py
+++ cityslam/mapping/single_video_pipeline.py
@@ -5,7 +5,6 @@
 
 from pathlib import Path
 import argparse
-import matplotlib.pyplot as plt
 from pycolmap import CameraMode
 
 from hloc import extract_features, match_features, reconstruction, visualization, pairs_from_retrieval
@@ -19,25 +18,15 @@ from hloc import pairs_from_sequence
 confs = {'pairing': ['sequential', 'retrieval', 'sequential+retrieval']}
 
 
-def main(base_dir, dataset, outputs, window_size, num_loc, pairing, run_reconstruction):
+def main(images, outputs, window_size, num_loc, pairing, run_reconstruction):
 
-    # define paths
-    images = base_dir / dataset / 'images-fps2'
-    outputs = base_dir / outputs
-
-    sfm_pairs = outputs / f'pairs-netvlad{num_loc}.txt'
-    sfm_dir = outputs / 'sfm_superpoint+superglue'
+    sfm_dir = outputs / 'sfm_sp+sg'
 
     retrieval_conf = extract_features.confs['netvlad']
     feature_conf = extract_features.confs['superpoint_aachen']
     matcher_conf = match_features.confs['superglue']
 
     outputs.mkdir(exist_ok=True, parents=True, mode=0o777)
-    # ref_sfm = outputs / 'sfm_superpoint+superglue'
-    # ref_sfm_scaled = outputs / 'sfm_sift_scaled'
-    # query_list = outputs / 'query_list_with_intrinsics.txt'
-    # sfm_pairs = outputs / f'pairs-db-covis{num_covis}.txt'
-    # loc_pairs = outputs / f'pairs-query-netvlad{num_loc}.txt'
 
     # ## Find image pairs either via sequential pairing, image retrieval or eventually both
 
@@ -51,21 +40,28 @@ def main(base_dir, dataset, outputs, window_size, num_loc, pairing, run_reconstr
         if pairing == 'sequential':
             image_list = get_image_names(images)
 
+            sfm_pairs = outputs / f'pairs-sequential{window_size}.txt'
+
             pairs_from_sequence.main(
-                sfm_pairs, image_list, features=None, window_size=window_size, quadratic=True, retrieval_interval=5)
+                sfm_pairs, image_list, features=None, window_size=window_size, quadratic=True)
         elif pairing == 'retrieval':
             # We extract global descriptors with NetVLAD and find for each image the most similar ones.
-            # TODO increase number of num mathced
             retrieval_path = extract_features.main(
                 retrieval_conf, images, outputs)
-            # TODO make a new pairs_from_retrieval that takes sequenciality (±10 images) and also does retrieval but NOT on the pairs already found from sequenciality
+
+            sfm_pairs = outputs / f'pairs-retrieval-netvlad{num_loc}.txt'
+
             pairs_from_retrieval.main(
                 retrieval_path, sfm_pairs, num_matched=num_loc)
+
         elif pairing == 'sequential+retrieval':
+            # We extract global descriptors with NetVLAD and find for each image the most similar ones.
             retrieval_path = extract_features.main(
                 retrieval_conf, images, outputs)
 
             image_list = get_image_names(images)
+
+            sfm_pairs = outputs / f'pairs-sequential{window_size}-retrieval-netvlad{num_loc}.txt'
 
             pairs_from_sequence.main(
                 sfm_pairs, image_list, features=None, window_size=window_size,
@@ -85,46 +81,50 @@ def main(base_dir, dataset, outputs, window_size, num_loc, pairing, run_reconstr
 
     # TODO add camera mode as a param, single works for now, but maybe per folder would be better when we start merging
     model = reconstruction.main(
-        sfm_dir, images, sfm_pairs, feature_path, match_path, camera_mode=CameraMode.SINGLE, run_reconstruction=run_reconstruction)
+        sfm_dir, images, sfm_pairs, feature_path, match_path, camera_mode=CameraMode.SINGLE, run=run_reconstruction)
 
-    return model, outputs, images
+    return model
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_dir', type=Path,
-                        default='/cluster/project/infk/courses/252-0579-00L/group07',
-                        help='base directory for datasets and outputs, default: %(default)s')
-    parser.add_argument('--dataset', type=Path, default='datasets/tiny_walk_zurich',
+    parser.add_argument('--dataset', type=Path, default='/cluster/project/infk/courses/252-0579-00L/group07/datasets/images/W25QdyiFnh0',
                         help='Path to the dataset, default: %(default)s')
-    parser.add_argument('--outputs', type=Path, default='outputs/tiny_walk_zurich',
+    parser.add_argument('--outputs', type=Path, default='/cluster/project/infk/courses/252-0579-00L/group07/datasets/outputs/W25QdyiFnh0',
                         help='Path to the output directory, default: %(default)s')
-    parser.add_argument('--window_size', type=int, default=2,
-                        help="Size of the window of images to match, default: %(default)s")
-    parser.add_argument('--num_loc', type=int, default=2,
-                        help='Number of image pairs for loc, default: %(default)s')
+    parser.add_argument('--window_size', type=int, default=6,
+                        help="Size of the window of images to match sequentially, default: %(default)s")
+    parser.add_argument('--num_loc', type=int, default=7,
+                        help='Number of image pairs for retrieval, default: %(default)s')
+    parser.add_argument('--retrieval_interval', type=int, default=5,
+                        help='How often to trigger retrieval: %(default)s')
     parser.add_argument('--pairing', type=str, default='sequential+retrieval',
                         help=f'Pairing method, default: %(default)s', choices=confs['pairing'])
     parser.add_argument('--run_reconstruction', action="store_true",
                         help="If we want to run the pycolmap reconstruction or not")
     args = parser.parse_args()
-    # ## Run mapping
+    
+    # Run mapping
     model, outputs, images = main(**args.__dict__)
 
-    # ## Visualization
-    # We visualize some of the registered images, and color their keypoint by visibility, track length, or triangulated depth.
+    if model is not None:
+        # We visualize some of the registered images, and color their keypoint by visibility, track length, or triangulated depth.
+        
+        print("Plotting some examples of sfm points")
+        plt_dir = outputs / 'plots'
+        plt_dir.mkdir(exist_ok=True, parents=True)
 
-    plt_dir = outputs / 'plots'
-    plt_dir.mkdir(exist_ok=True, parents=True)
+        visualization.visualize_sfm_2d(model, images, color_by='visibility', n=5)
+        viz.save_plot(plt_dir / 'visibility.png')
+        # plt.show()
 
-    visualization.visualize_sfm_2d(model, images, color_by='visibility', n=5)
-    viz.save_plot(plt_dir / 'visibility.png')
-    # plt.show()
+        visualization.visualize_sfm_2d(model, images, color_by='track_length', n=5)
+        viz.save_plot(plt_dir / 'track_length.png')
+        # plt.show()
 
-    visualization.visualize_sfm_2d(model, images, color_by='track_length', n=5)
-    viz.save_plot(plt_dir / 'track_length.png')
-    # plt.show()
-
-    visualization.visualize_sfm_2d(model, images, color_by='depth', n=5)
-    viz.save_plot(plt_dir / 'depth.png')
-    # plt.show()
+        visualization.visualize_sfm_2d(model, images, color_by='depth', n=5)
+        viz.save_plot(plt_dir / 'depth.png')
+        # plt.show()
+    
+    else:
+        print("Model is not created!\n Run hloc or colmap reconstruction!")
--- scripts/video_pipeline_colmap.sh
+++ scripts/video_pipeline_colmap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 BASE=/cluster/project/infk/courses/252-0579-00L/group07
 PAIRING=sequential+retrieval
-DATASET=4k/long_walk_zurich
+VIDEO=W25QdyiFnh0
 
 # Create log directory if it does not exist
-mkdir -p ./logs/${DATASET}
+mkdir -p ./logs/${VIDEO}
 
 # Run hloc pipeline, extracting features, pairing, matching...
-bsub -J hloc_pipeline -o ./logs/${DATASET}/test_${PAIRING}_fps2_%J.out < ./scripts/single_video_pipeline.sh
+bsub -J hloc_pipeline -o ./logs/${VIDEO}/${PAIRING}_%J.out < ./scripts/single_video_pipeline.sh
 
 # Run faster hierarchical mapper for reconstruction
-bsub -J hierarchical_mapper -w "ended(hloc_pipeline)" -o ./logs/${DATASET}/test_${PAIRING}_fps2_colmap_hier_mapping_%J.out < ./scripts/colmap_hier_mapper.sh
+bsub -J hierarchical_mapper -w "ended(hloc_pipeline)" -o ./logs/${VIDEO}/${PAIRING}_colmap_hier_mapping_%J.out < ./scripts/colmap_hier_mapper.sh
