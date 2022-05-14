fe85eda8253854333330e5229a52e25e225547a3
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-05-14 15:55:51 +0200

    added multiprocessing of videos

--- cityslam/preprocessing/preprocessing.py
+++ cityslam/preprocessing/preprocessing.py
@@ -3,6 +3,8 @@ import argparse
 import os
 
 import ffmpeg
+import multiprocessing as mp
+from functools import partial
 
 from .. import logger
 
@@ -27,28 +29,44 @@ def frame_capture(video_path, images_path, prefix="", fps=2, start='00:00:00', d
         raise e
 
 
+def frame_mp(file, videos, output, video_ids=None, overwrite=False, fps=2, start='00:00:00', duration='00:00:00'):
+    
+    video_id = file[file.find("[")+1:file.find("]")]
+    
+    # skip videoes not in video_ids
+    if video_ids is not None and video_id not in video_ids:
+        return None
+
+    logger.info(
+        f"extracting frames from video: {file} using fps: {fps}")
+    video_path = videos / file  # video_path
+    images_path = output / f"{video_id}"
+    
+    images_path.mkdir(parents=True, exist_ok=True)
+    if list(images_path.glob(f"*_fps{fps}_*.jpg")) and not overwrite:
+        logger.info(f"frames already extracted for video: {file}")
+        return None
+    frame_capture(video_path, images_path, prefix=video_id,
+                    fps=fps, start=start, duration=duration)
+
+    return images_path
+
+
 def main(videos, output, video_ids=None, overwrite=False, fps=2, start='00:00:00', duration='00:00:00'):
 
-    image_dirs = []
-    for file in os.listdir(videos):
-        if file.endswith((".mp4", "webm")):
-            video_id = file[file.find("[")+1:file.find("]")]
-            
-            # skip videoes not in video_ids
-            if video_ids is not None and video_id not in video_ids:
-                continue
+    files = [file for file in os.listdir(videos) if file.endswith((".mp4", "webm"))]
 
-            logger.info(
-                f"extracting frames from video: {file} using fps: {fps}")
-            video_path = videos / file  # video_path
-            images_path = output / f"{video_id}"
-            image_dirs.append(images_path)
-            images_path.mkdir(parents=True, exist_ok=True)
-            if list(images_path.glob(f"*_fps{fps}_*.jpg")) and not overwrite:
-                logger.info(f"frames already extracted for video: {file}")
-                continue
-            frame_capture(video_path, images_path, prefix=video_id,
-                          fps=fps, start=start, duration=duration)
+    with mp.Pool(min(mp.cpu_count(),8)) as p:
+        image_dirs = p.map(partial(frame_mp, 
+            videos=videos, 
+            output=output, 
+            video_ids=video_ids, 
+            overwrite=overwrite, 
+            fps=fps, start=start, 
+            duration=duration),
+        files)
+
+    image_dirs = [str(img_dir) for img_dir in image_dirs if img_dir is not None]
 
     return image_dirs
 
