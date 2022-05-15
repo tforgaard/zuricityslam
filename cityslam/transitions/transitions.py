87a7c732f787c4faa61faa79ded2c13e91bcdda3
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-05-15 17:39:02 +0200

    added transition True/False to cropped output, fix wierd fps bug, allow for running on cpu, add option for processing more files

--- cityslam/transitions/transitions.py
+++ cityslam/transitions/transitions.py
@@ -4,16 +4,16 @@ from pathlib import Path
 import numpy as np
 import ffmpeg
 import torch
-from transnetv2_pytorch import TransNetV2
+from .transnetv2_pytorch import TransNetV2
 
 """
 Adapted code from https://github.com/soCzech/TransNetV2/
 """
 
-def predict_raw(frames: np.ndarray, model):
+def predict_raw(frames: np.ndarray, model, device):
     with torch.no_grad():
         video_torch = torch.from_numpy(frames)
-        single_frame_pred, all_frame_pred = model(video_torch.cuda())
+        single_frame_pred, all_frame_pred = model(video_torch.to(device))
 
         single_frame_pred = torch.sigmoid(single_frame_pred).cpu().numpy()
         all_frame_pred = torch.sigmoid(all_frame_pred["many_hot"]).cpu().numpy()
@@ -21,7 +21,7 @@ def predict_raw(frames: np.ndarray, model):
 
         return single_frame_pred, all_frame_pred
 
-def predict_frames(frames: np.ndarray, model):
+def predict_frames(frames: np.ndarray, model, device):
         def input_iterator():
             # return windows of size 100 where the first/last 25 frames are from the previous/next batch
             # the first and last window must be padded by copies of the first and last frame of the video
@@ -43,7 +43,7 @@ def predict_frames(frames: np.ndarray, model):
         predictions = []
 
         for inp in input_iterator():
-            single_frame_pred, all_frames_pred = predict_raw(inp, model)
+            single_frame_pred, all_frames_pred = predict_raw(inp, model, device)
             predictions.append((single_frame_pred[0, 25:75, 0],
                                 all_frames_pred[0, 25:75, 0]))
 
@@ -77,13 +77,20 @@ def predictions_to_scenes(predictions: np.ndarray, threshold: float = 0.5):
 
         return np.array(scenes, dtype=np.int32)
 
-def add_max_min_cuts(video_file_path, max_scene_length, min_scene_length, cut_file):
+def add_max_min_cuts(video_file_path, max_scene_length, min_scene_length, cut_file, fps=2):
     probe = ffmpeg.probe(video_file_path)
     video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
-    fps = int(video_info['r_frame_rate'].split('/')[0])
+    video_fps = float(video_info['r_frame_rate'].split('/')[0])
+    
+    # Sometimes the frame rate is given as frames per 1000 seconds?
+    if video_fps / 1000 > 1.0:
+        video_fps = video_fps / 1000
+    
+    print(f"video fps: {video_fps}")
+    max_frames = int(video_fps*max_scene_length)
+    min_frames = int(video_fps*min_scene_length)
 
-    max_frames = fps*max_scene_length
-    min_frames = fps*min_scene_length
+    fps_ratio = video_fps / fps
 
     """
     we drop too short scenes,
@@ -98,61 +105,82 @@ def add_max_min_cuts(video_file_path, max_scene_length, min_scene_length, cut_fi
     new_scenes = []
     with open(cut_file, 'r') as file:
         scenes = np.loadtxt(file, dtype=int)
-        for i, scene in enumerate(scenes):
-            scene_length = scene[1] - scene[0]
+        for i, (scene_start, scene_end) in enumerate(scenes):
+            scene_length = scene_end - scene_start
             whole_maxes = int(scene_length // max_frames)
             rest_maxes = int(scene_length % max_frames)
-            
+
             if scene_length > min_frames:
                 
                 for j in range(whole_maxes):
-                    new_scenes.append([scene[0] + j*max_frames, scene[0] + (j+1)*max_frames - 1])
-                
+                    transition = j == 0 and i != 0
+                    new_scenes.append([int((scene_start + j*max_frames) // fps_ratio), int((scene_start + (j+1)*max_frames - 1) // fps_ratio), transition])
+
                 if rest_maxes >= min_frames:
-                    new_scenes.append([scene[0] + (whole_maxes)*max_frames, scene[1]])
+                    transition = whole_maxes == 0
+                    new_scenes.append([int((scene_start + (whole_maxes)*max_frames) // fps_ratio), int(scene_end // fps_ratio), transition])
         
-    with open(cut_file + "_cropped", 'w') as file:
+    with open(f"{cut_file.parent / cut_file.stem}_cropped.txt", 'w') as file:
             np.savetxt(file, new_scenes, fmt="%d")
 
 
-def main(video_file_path, model_path, output, max_scene_length, min_scene_length):
-    assert video_file_path.is_file()
+def main(videos_dir, video_ids, model_path, output, max_scene_length, min_scene_length, fps, threshold, overwrite=False):
+    videos_dir = Path(videos_dir)
+    assert videos_dir.exists(), videos_dir    
 
-    output_file = output / video_file_path.stem
+    model = TransNetV2()
+    model_weights = Path(model_path) / "transnetv2-pytorch-weights.pth"
+    assert model_weights.exists(), model_weights
+    state_dict = torch.load(str(model_weights))
+    model.load_state_dict(state_dict)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print(f"using device {device}")
+    model.eval().to(device)
 
-    if not output_file.exists():
-        model = TransNetV2()
-        state_dict = torch.load("transnetv2-pytorch-weights.pth")
-        model.load_state_dict(state_dict)
-        model.eval().cuda()
+    if video_ids is None:
+        video_ids = [file[file.find("[")+1:file.find("]")] for file in videos_dir.iterdir()]
 
-        video_stream, err = ffmpeg.input(video_file_path).output(
-                "pipe:", format="rawvideo", pix_fmt="rgb24", s="48x27"
-            ).run(capture_stdout=True, capture_stderr=True)
+    for video_id in video_ids:
 
-        video = np.frombuffer(video_stream, np.uint8).reshape([-1, 27, 48, 3])
+        video_file_path = next(videos_dir.glob(f"*{video_id}*"))
+        if not video_file_path:
+            print(f"could not find video with id {video_id}, skipping")
+            continue
 
-        single_frame_pred, all_frames_pred = predict_frames(video, model)
+        output_file = Path(output) / f"{video_id}_transitions.txt"
+        output_file.parent.mkdir(exist_ok=True, parents=True)
+        
+        if not output_file.exists() or overwrite:
 
-        scenes = predictions_to_scenes(single_frame_pred)
+            video_stream, err = ffmpeg.input(video_file_path).output(
+                    "pipe:", format="rawvideo", pix_fmt="rgb24", s="48x27"
+                ).run(capture_stdout=True, capture_stderr=True)
 
-        with open(output_file, 'w') as file:
-            np.savetxt(file, scenes, fmt="%d")
+            video = np.frombuffer(video_stream, np.uint8).reshape([-1, 27, 48, 3])
 
-    else:
-        print(f"Already found cuts for video {video_file_path.stem}")
+            print(f"Finding transitions for video {video_file_path.stem}")
+            single_frame_pred, all_frames_pred = predict_frames(video, model, device)
 
-    add_max_min_cuts(video_file_path, max_scene_length, min_scene_length, output_file)
+            scenes = predictions_to_scenes(single_frame_pred, threshold)
 
+            with open(output_file, 'w') as file:
+                np.savetxt(file, scenes, fmt="%d")
 
+        else:
+            print(f"Already found transitions for video {video_file_path.stem}")
+
+        add_max_min_cuts(video_file_path, max_scene_length, min_scene_length, output_file, fps)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--video_file_path', type=Path,
+    parser.add_argument('--videos_dir', type=Path,
                         default='/cluster/home/ksteinsland/zuricityslam/base/kriss/datasets/videos/🇨🇭 Zürich Switzerland Walk 4K 🌁 4K Walking Tour ☁️ (Cloudy Day) 🇨🇭 [W25QdyiFnh0].mp4',
-                        help='path to video')
+                        help='path to videos')
+    parser.add_argument('--video_ids', type=str, nargs="+",
+                            default=None,
+                        help='video_id')
     parser.add_argument('--model_path', type=str,
-                        default='/cluster/project/infk/courses/252-0579-00L/group07/data/dev/TransNetV2/inference/transnetv2-weights',
+                        default='./', #'/cluster/project/infk/courses/252-0579-00L/group07/data/dev/TransNetV2/inference/transnetv2-weights',
                         help='path to transiton detection model weights')
     parser.add_argument('--output', type=Path,
                         default='/cluster/project/infk/courses/252-0579-00L/group07/kriss/datasets/cuts',
@@ -163,6 +191,13 @@ if __name__ == "__main__":
     parser.add_argument('--min_scene_length', type=int,
                         default=30,
                         help='number of seconds of min scene length')
+    parser.add_argument('--fps', type=int,
+                        default=2,
+                        help='fps of output cropped file')
+    parser.add_argument('--threshold', type=float,
+                        default=0.5,
+                        help='transition threshold')
+    parser.add_argument('--overwrite', action="store_true")
 
     args = parser.parse_args()
     main(**args.__dict__)
