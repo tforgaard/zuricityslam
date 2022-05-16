1be7b665b87e72f8c6c7e53f5815f500484e38a8
ksteinsland <ksteinsland@student.ethz.ch>
2022-05-16 11:53:10 +0200

    run om low quality and fixed zero transitions bug

--- scripts/find_transitions.sh
+++ scripts/find_transitions.sh
@@ -10,10 +10,15 @@
 source ./scripts/colmap_startup.sh
 
 BASE=/cluster/project/infk/courses/252-0579-00L/group07
-VIDS_PATH=${BASE}/datasets/videos
+VIDS_PATH=${BASE}/datasets/videos_wv
 TRANS_PATH=${BASE}/datasets/transitions
 
 VIDEO_IDS="W25QdyiFnh0 s5MlvdGL8Lg fGxbtg1ytJo uVCG0MAkW0E ctYA9Fzo6WM vl_BfAZf4LY KzrCHdviKho yTSf9vI8nI4 DdxEr70jC6E Ka3Ss3cdk0Y N1FXwkV0MMY _NmYvuEILw4 m3EbUAoyWHo lN4j2iiFpgQ ITntTt4qkWY dzJV1iG8C3A 2obsKLoZQdU EctFmZiZiEs z6q7PG_pTEs jsOKs7cnoI4 73IVzh0R-Lo NpQqe_nLWjg Rq2VnLu5aTw 207zUTmTiBc t1DpmdNq6QA Jm3eNoYT_vY _k_mojbxh_A ZQRpwAJPfQo xaMI2KnNS4Y 28vkHTRVeqo tENRFDzrim4 jjk1IEryf7Q zjGlPabjZPE ITo-ZFQWSYM CFanF4f0fs4 OSMTFYGVLyE KjEo9sf5y-I ENDo7IPlgMg w4aMLRGpMEw NFGvl2XeOxI QGCTwz1Kqe0 _jJGc4r1mzk ziTGhoM0SOU 453esepQAmQ P6Rc9zao3wo cdzf6Tw7bfA 9Xr-rNyLIjk 606xkPY56R4 ElVdGIWdteI ZpJmSnOTKG0 tmDFCotvcoQ K2rlPMRUYKI"
+
+#we run the transition finder on low quality versions for speedup
+python3 -m cityslam.videointerface.downloader   ${VIDEO_IDS} \
+                                                --output ${VIDS_PATH} \
+                                                --format 'wv'
 
 python3 -m cityslam.transitions.transitions     --videos_dir ${VIDS_PATH} \
                                                 --video_ids  ${VIDEO_IDS} \
