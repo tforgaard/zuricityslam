7630f4a6d6b611feb18d2a6244516ed7aa89c6c7
Theodor Forgaard <tforgaard@student.ethz.ch>
2022-05-16 22:31:16 +0200

    wrote some code for safely running parallel batch jobs (hopefully)

--- pipeline_matching.py
+++ pipeline_matching.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from filelock import Timeout, FileLock
 
 from cityslam.mapping import single_video_pipeline
 
@@ -13,13 +14,15 @@ base_dir = Path('/cluster/project/infk/courses/252-0579-00L/group07')
 
 images_path = base_dir / 'datasets' / 'images'
 image_splits = base_dir / 'datasets' / 'image_splits'
-output_path = base_dir / 'outputs' / 'models-split-testing'
+output_path = base_dir / 'outputs' / 'reconstructions'
 
 # scene_ids = ['_jJGc4r1mzk_part0']
 scene_ids = [p.name.split("_images")[0] for p in sorted(list(image_splits.iterdir()))][:8]
 
 
 for scene_id in scene_ids:
-    reconstruction = single_video_pipeline.main(
-        images_path, image_splits, output_path, video_id=scene_id, window_size=6, num_loc=6, pairing='sequential+retrieval', run_reconstruction=False)
-
+    lock_path = output_path / f"{scene_id}.lock"
+    lock = FileLock(lock_path, timeout=5)
+    with lock:
+        reconstruction = single_video_pipeline.main(
+            images_path, image_splits, output_path, video_id=scene_id, window_size=6, num_loc=6, pairing='sequential+retrieval', run_reconstruction=False)
