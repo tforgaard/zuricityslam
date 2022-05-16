834256a089690ec87e5c6daff763e8554987046c
ksteinsland <ksteinsland@student.ethz.ch>
2022-05-16 16:19:42 +0200

    deleted old files

--- scripts/cuts.sh
+++
@@ -1,9 +0,0 @@
-
-#source ./scripts/colmap_startup.sh
-
-#module load gcc/8.2.0 cuda/11.2.2 cudnn/8.2.4.15 ffmpeg
-#conda install tensorflow
-#bsub -I -n 1 -W 1:00 -R "rusage[mem=2048, ngpus_excl_p=1]" 'python -c ""'
-
-bsub -I -n 16 -W 1:00 -R "rusage[mem=4096, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=4096]" python cityslam/videointerface/transition_cut.py
-
