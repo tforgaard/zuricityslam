67472c1fb0e0ee7c9a44c26cf0f76943e089f581
Senthuran Kalananthan <skalanan@eu-login-20.euler.ethz.ch>
2022-05-19 11:26:03 +0200

    add score_pipeline.py

--- cityslam/localization/pipeline_abs_pose_estimation.py
+++ cityslam/localization/pipeline_abs_pose_estimation.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
+
 from pathlib import Path
-from helper_functions import get_images_from_recon, create_query_file, RANSAC_Transformation
+from helper_functions import get_images_from_recon, create_query_file, RANSAC_Transformation, model_path_2_name, model_name_2_path
 import argparse
 
 from hloc import extract_features, match_features
@@ -10,13 +12,22 @@ import pycolmap
 def main(images, models, output_dir, num_loc, N, reference, target, max_it, scale_std, max_distance_error, max_angle_error):
     
     # Setup the paths
-    outputs = output_dir / 'merge' / f'merge_{target}_{reference}'
+    """
+    target_path = model_name_2_path(target)
+    reference_path = model_name_2_path(reference)
 
+    target_name = model_path_2_name(target)
+    reference_name = model_path_2_name(reference)
+    """
+    target_name = target
+    reference_name = reference
+    
+    outputs = output_dir / 'merge' / f'merge_{target_name}_{reference_name}'
     outputs.mkdir(exist_ok=True, parents=True)
 
     # This is the reference and target model path
-    reference_sfm = models / reference / 'sfm_sp+sg'  # Replace this to one path 
-    target_sfm = models / target / 'sfm_sp+sg'        # Replace this to one path
+    reference_sfm = models / reference / 'sfm_sp+sg'  
+    target_sfm = models / target / 'sfm_sp+sg'        
 
     # Retrieval pairs from target to reference
     loc_pairs = outputs / f'pairs-query-netvlad{num_loc}.txt'  # top-k retrieved by NetVLAD
@@ -36,15 +47,15 @@ def main(images, models, output_dir, num_loc, N, reference, target, max_it, scal
     # Path to the global descriptors
     #global_descriptors = extract_features.main(retrieval_conf, images, outputs)
     global_descriptors = models / 'global-feats-netvlad.h5'
-
+    
     # Get images from target model
     query_images = get_images_from_recon(target_sfm)
 
     # Use a subset of the images from the target model as queries
     query_list = query_images[::N]
-
+    
     # Create a text file containing query images names and camera parameters
-    queries_file = outputs / f'{target}_queries_with_intrinsics.txt'
+    queries_file = outputs / f'{target_name}_queries_with_intrinsics.txt'
     create_query_file(target_sfm, query_list, queries_file)
     
     # Do retrieval from target-queries to reference 
@@ -74,21 +85,40 @@ def main(images, models, output_dir, num_loc, N, reference, target, max_it, scal
 
     # TODO: safe best transform
     print(best_transform)
-    return best_transform
+    
+    reference_model = pycolmap.Reconstruction(reference_sfm)
+    target_model_transformed = pycolmap.Reconstruction(target_sfm)
+    target_model_transformed.transform(best_transform)
+
+    fig = viz_3d.init_figure()
+    viz_3d.plot_reconstruction(fig, reference_model, color='rgba(255,0,0,0.2)', name="reference")
+    viz_3d.plot_reconstruction(fig, target_model_transformed, color='rgba(0,255,0,0.2)', name="target transformed")
+
+    fig.write_html(f'{outputs}/reconstruction_{target_name}_{reference_name}.html')
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--images', type=Path, default='/cluster/project/infk/courses/252-0579-00L/group07/datasets/images',
                         help='Path to the dataset, default: %(default)s')
+    """
+    parser.add_argument('--models', type=Path, default='/cluster/project/infk/courses/252-0579-00L/group07/outputs/models-merge-testing-sent/model-matches-testing',
+                        help='Path to the model directory, default: %(default)s')  
+    """
     parser.add_argument('--models', type=Path, default='/cluster/project/infk/courses/252-0579-00L/group07/outputs/models-merge-testing-sent/models-merge-testing',
-                        help='Path to the model directory, default: %(default)s')   
+                        help='Path to the model directory, default: %(default)s')
     parser.add_argument('--output_dir', type=Path, default='/cluster/home/skalanan/',
                         help='Path to the output directory, default: %(default)s')                   
     parser.add_argument('--num_loc', type=int, default=10,
                         help='Number of retrieval pairs to generate for each query image: %(default)s')
     parser.add_argument('--N', type=int, default=5,
                         help='Use every Nth image from the images in the target reconstruction as query image: %(default)s')
+    """                    
+    parser.add_argument('--reference', type=str, default='TZIHy1cZJ-U/part0',
+                        help='video id for reference model, %(default)s')
+    parser.add_argument('--target', type=str, default='gTHMvU3XHBk/part0',
+                        help='video id for target model, %(default)s')
+    """
     parser.add_argument('--reference', type=str, default='TZIHy1cZJ-U',
                         help='video id for reference model, %(default)s')
     parser.add_argument('--target', type=str, default='gTHMvU3XHBk',
---
+++ cityslam/localization/score_pipeline.py
@@ -0,0 +1,71 @@
+import json
+import argparse
+from pathlib import Path
+import pipeline_abs_pose_estimation
+
+def main(scores_file, images, models, output_dir, num_loc, N, reference, target, max_it, scale_std, max_distance_error, max_angle_error):
+    
+    with open(scores_file) as f:
+        score_dict = json.load(f)
+
+    score_items = score_dict.items()
+    tuple_pair = []
+    for item in score_items:
+        reference = (item[0],)
+        sub_dict = item[1]
+        tuple_pair += [reference+dict for dict in sub_dict.items()]
+
+    sorted_scores = sorted(tuple_pair, key=lambda item: item[2], reverse=True)
+    print(sorted_scores)
+
+    for (reference, target, score) in sorted_scores:
+        if(score == 0.0): 
+            continue
+
+        reference_name = str(reference).replace('__part0__sfm_sp+sg', '')
+        target_name = str(target).replace('__part0__sfm_sp+sg', '')
+
+        pipeline_abs_pose_estimation.main(images, models, output_dir, num_loc, N, reference_name, target_name, max_it, scale_std, max_distance_error, max_angle_error)
+
+    """
+    TODO:
+    - check if the keys of score.json is the reference or target 
+    - input correct file format with part0 stuff
+    - Do we want to iterate over each pair and get partial reconstructions (& do this multiple times) 
+        or do we want to merge the first pairs and update the list & use this model to build a bigger map? (instead of doing this multiple times)?
+    - when creating the score.json file ignore pairs with itself?
+    """
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--scores_file', type=Path, default='/cluster/project/infk/courses/252-0579-00L/group07/theo/outputs/model-matches-testing/merge/model_match_scores.json',
+                        help='Path to the scores file, default: %(default)s')
+    parser.add_argument('--images', type=Path, default='/cluster/project/infk/courses/252-0579-00L/group07/datasets/images',
+                        help='Path to the dataset, default: %(default)s')
+    parser.add_argument('--models', type=Path, default='/cluster/project/infk/courses/252-0579-00L/group07/outputs/models-merge-testing-sent/models-merge-testing',
+                        help='Path to the model directory, default: %(default)s')   
+    parser.add_argument('--output_dir', type=Path, default='/cluster/home/skalanan/',
+                        help='Path to the output directory, default: %(default)s')                   
+    parser.add_argument('--num_loc', type=int, default=10,
+                        help='Number of retrieval pairs to generate for each query image: %(default)s')
+    parser.add_argument('--N', type=int, default=5,
+                        help='Use every Nth image from the images in the target reconstruction as query image: %(default)s')
+    parser.add_argument('--reference', type=str, default='TZIHy1cZJ-U/part0',
+                        help='video id for reference model, %(default)s')
+    parser.add_argument('--target', type=str, default='gTHMvU3XHBk/part0',
+                        help='video id for target model, %(default)s')
+    parser.add_argument('--max_it', type=int, default=200,
+                        help='Max iteration for RANSAC: %(default)s')
+    parser.add_argument('--scale_std', type=float, default=0.15306122448979592,
+                        help='Max iteration for RANSAC: %(default)s')
+    parser.add_argument('--max_distance_error', type=int, default=3,
+                        help='Max iteration for RANSAC: %(default)s')
+    parser.add_argument('--max_angle_error', type=int, default=5,
+                        help='Max iteration for RANSAC: %(default)s')                    
+    args = parser.parse_args()
+    
+    # Run mapping
+    model = main(**args.__dict__)
